b6e59f9ccb442ddd4544028d5395378aab447b57
Eduardo De La Cruz <eduardo.delacruz@gft.com>
2019-12-30 12:54:17 +0100

    Initial Commit

---
+++ GeeksHubsTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GeeksHubsTest
+//
+//  Created by De La Cruz, Eduardo on 30/12/2019.
+//  Copyright © 2019 De La Cruz, Eduardo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
